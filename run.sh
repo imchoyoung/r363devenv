a0f4d05e82dd4948fef6aeae513429762fa46e42
Choyoung Yim <alperceire@pm.me>
2020-10-31 08:32:29 -0300

    check if there is change in the build

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if docker ps | grep rdevenv; then docker stop rdevenv; fi
-
+docker build  --tag alperceire/rdevenv .
 RDEV_HOST=${HOME}/rey
 RENV_PATHS_ROOT_HOST=${HOME}/caque/renv
 RENV_PATHS_ROOT_CONTAINER=/home/rstudio/renv
