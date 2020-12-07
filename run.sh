b94a70040e3ef350a30e282b738bd1379cf7f81f
Choyoung Yim <alperceire@pm.me>
2020-12-07 12:13:55 -0300

    generalise name convention

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 if docker ps | grep rdevenv; then docker stop rdevenv; fi
 # docker build  --tag alperceire/rdevenv .
-RDEV_HOST_HOME=${HOME}/caque
+RDEV_HOST_HOME=${HOME}/rstudio_docker
 RDEV_CONT_HOME=/home/rstudio
-RDEV_HOST_GIT=${HOME}/rey
-RDEV_CONT_GIT=${RDEV_CONT_HOME}/rey
-# RENV_PATHS_ROOT_HOST=${HOME}/caque/renv
-# RENV_PATHS_ROOT_CONTAINER=/home/rstudio/.local/share/renv
+RDEV_HOST_GIT=${HOME}/git_project
+RDEV_CONT_GIT=${RDEV_CONT_HOME}/git_project
 
 docker run --rm -d \
     --name rdevenv \
@@ -15,4 +13,4 @@ docker run --rm -d \
     -v "${RDEV_HOST_HOME}:${RDEV_CONT_HOME}" \
     -v "${RDEV_HOST_GIT}:${RDEV_CONT_GIT}" \
     -p 127.0.0.1:8787:8787 \
-    alperceire/rdevenv:test
+    alperceire/rdevenv
