0409afc142e721af6527e96b478eaee90963a947
alperceire <alperceire@pm.me>
2021-03-21 21:42:14 -0300

    fix timezone information to propagate until R env

---
+++ r4/runr4.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if docker ps | grep devr4; then docker stop devr4; fi
+# docker build  --tag alperceire/devr4 .
+RDEV_HOST_HOME=${HOME}/caque
+RDEV_CONT_HOME=/home/rstudio
+RDEV_HOST_GIT=${HOME}/rey
+RDEV_CONT_GIT=${RDEV_CONT_HOME}/rey
+
+docker run --rm -d \
+    --name devr4 \
+    -e DISABLE_AUTH=true \
+    -e ROOT=TRUE \
+    -v "${RDEV_HOST_HOME}:${RDEV_CONT_HOME}" \
+    -v "${RDEV_HOST_GIT}:${RDEV_CONT_GIT}" \
+    -p 127.0.0.1:8787:8787 \
+    alperceire/devr4
